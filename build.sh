9929801e0667f75f59d25e57f32c0783d179ec4b
Natanael <natanaelgplay@gmail.com>
2023-09-11 13:06:53 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,25 @@
+# #!/bin/bash
+
+[ ! "$SUDO_USER" ] && {
+  echo "Execute esse script como root:"
+  echo
+  echo "  sudo $0"
+  echo
+  exit 1
+}
+
+caminho=$(pwd)
+
+mkdir build
+cp -vrp $caminho/DEBIAN/ $caminho/build/
+cp -vrp $caminho/usr/ $caminho/build/
+cp -vrp $caminho/etc/ $caminho/build/
+
+cd build
+
+sudo chown -R root:root $caminho/build/
+sudo chmod 755 -Rf $caminho/build/
+
+dpkg-deb -b $caminho/build/ $caminho
+
+mv $caminho/*.deb $caminho/alerta-kernel.deb
